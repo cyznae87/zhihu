558500e6ec1a596b3fad5e7ec7a88812ab4d2a8e
Shelley <shelley881022@gmail.com>
2019-12-30 22:27:50 +0800

    update main

--- main.py
+++ main.py
@@ -1,27 +1,57 @@
 import zhihuapi as api
 import csv
+import time
+import random
+
+
+def follow_and_record(follower_list):
+    with open('zhihuers.csv', mode='a', newline='') as zhihuers_file:
+        follower_writer = csv.writer(zhihuers_file)
+        for follower in follower_list:
+            time.sleep(round(random.uniform(0, 2), 2))
+            uid = follower['url_token']
+            name = follower['name']
+            print(uid)
+            try:
+                api.action.follow(uid)
+                follower_writer.writerow([uid, name, 'followed'])
+            except:
+                follower_writer.writerow([uid, name, 'error'])
+                continue
+
+
 
 with open('cookie') as f:
     api.cookie(f.read())
 
 data = api.user('zhihuadmin').profile()
-print(data)
+#print(data)
 
-offset = 20
-for i in range(3):
-    followers = api.question(22078840).followers(offset=offset)
-    offset += 20
 
-#print(followers)
+followers = api.question(22078840).followers()
+offset = len(followers)
 
-for follower in followers:
-    try:
-        uid = follower['url_token']
-        name = follower['name']
-        print(uid)
-        with open('zhihuers.csv', mode='w') as zhihuers_file:
-            employee_writer = csv.writer(zhihuers_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            employee_writer.writerow([uid, name])
-        api.action.follow(uid)
-    except:
-        continue
+follow_and_record(followers)
+
+while (len(followers)>0):
+    time.sleep(round(random.uniform(10, 30), 3))
+    followers = api.question(22078840).followers(offset)
+    offset += len(followers)
+    
+    print(followers)
+
+    follow_and_record(followers)
+
+
+
+# for follower in followers:
+#     try:
+#         uid = follower['url_token']
+#         name = follower['name']
+#         print(uid)
+#         with open('zhihuers.csv', mode='w') as zhihuers_file:
+#             employee_writer = csv.writer(zhihuers_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+#             employee_writer.writerow([uid, name])
+#         api.action.follow(uid)
+#     except:
+#         continue
